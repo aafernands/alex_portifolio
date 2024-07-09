0cbaa1e0782cd644ab631bf0411b10cf51997253
Alex Fernandes <alexfernands@outlook.com>
2024-07-08 22:38:24 -0400

    Updated package files and App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AppBar, Toolbar, Typography, Container, Button, Grid, Paper } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            My Portfolio
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Grid container spacing={3} style={{ marginTop: '20px' }}>
+          <Grid item xs={12}>
+            <Paper style={{ padding: '20px' }}>
+              <Typography variant="h4" component="h2" gutterBottom>
+                Welcome to My Portfolio
+              </Typography>
+              <Button variant="contained" color="primary">
+                Get Started
+              </Button>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
