ff88cb2cd2b58a0daf599c94d9c554415e0f4f3d
Alex Fernandes <alexfernands@outlook.com>
2024-07-09 23:31:15 -0400

    updates to home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Container, Button, Grid, Paper } from '@mui/material';
+import React from "react";
+import { Typography, Container, Button, Grid, Paper } from "@mui/material";
+import MyAppBar from "./components/AppBar"; // Import the AppBar component
 
 function App() {
   return (
     <div>
-      <AppBar position="static">
+      {/* <AppBar position="static">
         <Toolbar>
           <Typography variant="h6">
             My Portfolio
           </Typography>
         </Toolbar>
-      </AppBar>
+      </AppBar> */}
+
+      <MyAppBar />
+
       <Container>
-        <Grid container spacing={3} style={{ marginTop: '20px' }}>
+        <Grid container spacing={3} style={{ marginTop: "20px" }}>
           <Grid item xs={12}>
-            <Paper style={{ padding: '20px' }}>
+            <Paper style={{ padding: "20px" }}>
               <Typography variant="h4" component="h2" gutterBottom>
-                Welcome t My Portfolio
+                HI THERE! I'M ALEX FERNANDES
+              </Typography>
+              <Typography variant="body1" component="p" gutterBottom>
+              I'm a Web Developer student based in New Jersey, United States. I
+              strive to build immersive and beautiful web applications through
+              carefully crafted code and user-centric design.
               </Typography>
               <Button variant="contained" color="primary">
                 Get Started
