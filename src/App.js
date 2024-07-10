a1ca3bbd62f86cfe81f7a2bb9047824bdf653c07
Alex Fernandes <alexfernands@outlook.com>
2024-07-09 23:53:22 -0400

    addes other sections of main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Typography, Container, Button, Grid, Paper } from "@mui/material";
 import MyAppBar from "./components/AppBar"; // Import the AppBar component
+import { useRef } from 'react';
 
 function App() {
   return (
@@ -18,42 +19,63 @@ function App() {
       <Container>
         <Grid container spacing={3} style={{ marginTop: "20px" }}>
           <Grid item xs={12}>
-            <Paper style={{ padding: "20px" }}>
+            <Paper style={{ backgroundColor: "#DADDD8", padding: "20px" }}>
               <Typography variant="h4" component="h2" gutterBottom>
                 HI THERE! I'M ALEX FERNANDES
               </Typography>
               <Typography variant="body1" component="p" gutterBottom>
-              I'm a Web Developer student based in New Jersey, United States. I
-              strive to build immersive and beautiful web applications through
-              carefully crafted code and user-centric design.
+                I'm a Web Developer student based in New Jersey, United States.
+                I strive to build immersive and beautiful web applications
+                through carefully crafted code and user-centric design.
               </Typography>
               <Button variant="contained" color="primary">
                 PROJECTS
               </Button>
             </Paper>
-            
           </Grid>
 
           <Grid item xs={12}>
             <Paper style={{ padding: "20px" }}>
               <Typography variant="h4" component="h2" gutterBottom>
-                HI THERE! I'M ALEX FERNANDES
+                ABOUT{" "}
               </Typography>
               <Typography variant="body1" component="p" gutterBottom>
-              I'm a Web Developer student based in New Jersey, United States. I
-              strive to build immersive and beautiful web applications through
-              carefully crafted code and user-centric design.
+                I'm a Web Developer student based in New Jersey, United States.
+                I strive to build immersive and beautiful web applications
+                through carefully crafted code and user-centric design.
+              </Typography>
+             
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Paper style={{ padding: "20px" }}>
+              <Typography variant="h4" component="h2" gutterBottom>
+                PROJECTS{" "}
+              </Typography>
+              <Typography variant="body1" component="p" gutterBottom>
+                I'm a Web Developer student based in New Jersey, United States.
+                I strive to build immersive and beautiful web applications
+                through carefully crafted code and user-centric design.
+              </Typography>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Paper style={{ padding: "20px" }}>
+              <Typography variant="h4" component="h2" gutterBottom>
+                CONTACT{" "}
+              </Typography>
+              <Typography variant="body1" component="p" gutterBottom>
+                I'm a Web Developer student based in New Jersey, United States.
+                I strive to build immersive and beautiful web applications
+                through carefully crafted code and user-centric design.
               </Typography>
               <Button variant="contained" color="primary">
                 PROJECTS
               </Button>
             </Paper>
-            
           </Grid>
-
-
-
-
         </Grid>
       </Container>
     </div>
--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -9,9 +9,9 @@ import MenuIcon from "@mui/icons-material/Menu";
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1 }} >
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar style={{backgroundColor: "#1C1C1C"}}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             FNDS Labs
           </Typography>
