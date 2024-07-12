41c7c80c025a2e264ddd2824c9055e7bcdc99beb
Alex Fernandes <alexfernands@outlook.com>
2024-07-11 21:54:25 -0400

    adjusted style and added menu drawer

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -3,46 +3,33 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import logo from '../images/logo.png';
-import { makeStyles } from '@material-ui/styles';
-
-
+import logo from "../images/logo.png";
+import { makeStyles } from "@material-ui/styles";
+import MenuDrawer from "./MenuDrawer";
 
 const useStyles = makeStyles(() => ({
-   
   toolbar: {
-    minHeight: '36px', // Adjust the height as needed
-    justifyContent: 'center', // Center the content horizontally
-    backgroundColor: "#02182B"
+    minHeight: "36px",
+    justifyContent: "space-between", // Adjust this for spacing
+    backgroundColor: "#02182B",
   },
+  img: { height: "30px", marginRight: "10px" },
+  title: { flexGrow: 1, textAlign: "center" },
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
 
   return (
-    <Box sx={{ flexGrow: 1 }} >
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed" className={classes.appBar}>
-        
-      <Toolbar className={classes.toolbar}>
-      <img src={logo} alt="Logo" style={{ height: '30px', marginRight: '10px' }} />
-
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+        <Toolbar className={classes.toolbar}>
+          <img src={logo} alt="Logo" className={classes.img} />
+          <Typography variant="h5" component="div" className={classes.title}>
             F.N.D.S Labs
           </Typography>
-          {/* <Button color="inherit">Login</Button>{" "} */}
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <MenuDrawer />
         </Toolbar>
       </AppBar>
     </Box>
---
+++ src/components/MenuDrawer.js
@@ -0,0 +1,66 @@
+import * as React from "react";
+import { Drawer, List, ListItem, ListItemText, IconButton, Hidden, Box } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const drawerWidth = "100%";
+
+export default function MenuDrawer() {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <List>
+        {["HOME", "ABOUT", "PROJECTS", "CONTACT"].map((text) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <Box>
+      <Hidden mdUp>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="end"
+          onClick={handleDrawerToggle}
+          sx={{ ml: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          variant="temporary"
+          anchor="right"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Hidden>
+      <Hidden mdDown>
+        <Box sx={{ display: "flex" }}>
+          {["HOME", "ABOUT", "PROJECTS", "CONTACT"].map((text) => (
+            <Box key={text} sx={{ mx: 2 }}>
+              <ListItem button>
+                <ListItemText primary={text} />
+              </ListItem>
+            </Box>
+          ))}
+        </Box>
+      </Hidden>
+    </Box>
+  );
+}
