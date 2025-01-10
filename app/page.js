0a136ec771d1e74f64e610a5c0caf621660e1d28
Alex Fernandes <alexfernands@outlook.com>
2025-01-10 16:12:28 -0500

    Replace old files with Next.js app

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+
+  const currentDate = new Date().getFullYear()
+
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Welcome to Portfolio Page</h1>
+      </main>
+      <footer className={styles.footer}>
+        <p> © {currentDate} | FNDS Labs All rights reserved.
+        </p>
+      </footer>
+    </div>
+  );
+}
